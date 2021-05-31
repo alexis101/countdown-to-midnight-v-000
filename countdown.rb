b6e9142c612b744abd13453b0de7f71cf3c02f8b
Alexis Kondilis <kondilisalexis@gmail.com>
2021-05-31 04:34:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,6 @@
 #write your code here
 
+<<<<<<< HEAD
 def countdown(number)
 
   while number > 0
@@ -19,4 +20,13 @@ def countdown_with_sleep(number)
 end
 
   return "HAPPY NEW YEAR!"
+=======
+def countdown
+  number = 10
+  while number < 0
+    puts "#{number} SECOND(S) !"
+    number -= 1
+>>>>>>> 860891ca354acf10c70ae253bfca2d790fbc71d9
 end
+
+ puts "HAPPY NEW YEAR!"
